8c8af39e41b8d4099a801a0256013c03b3e8d190
nievesth <mary.nieves@bootcamp-upgrade.com>
2021-05-17 13:09:33 +0200

    ejercicio 02 for para reemplazar

---
+++ modulo01/extra/ejercicio02.js
@@ -0,0 +1,14 @@
+//Usa un for para remplazar todas las comidas que no sean veganas con las comidas del array de frutas. Recuerda no usar frutas duplicadas. Finalmente, imprime el array resultante.
+
+const fruits = ['Strawberry', 'Banana', 'Orange', 'Apple'];
+const foodSchedule = [{name: "Salad", isVegan: true},{name: "Salmon", isVegan: false}, {name: "Tofu", isVegan: true}, {name: "Burger", isVegan: false}, {name: "Rice", isVegan: true}, {name: "Pasta", isVegan: true}];
+
+
+for (let index = 0; index < foodSchedule.length; index++) {
+    const food = foodSchedule[index];
+    if(!food.isVegan){
+        food.name = fruits.shift();
+    }
+}
+
+console.log(foodSchedule);
