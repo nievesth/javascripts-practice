e8ab94e0db6823488ad2ffb50ffedb09199541e9
nievesth <mary.nieves@bootcamp-upgrade.com>
2021-05-17 12:05:48 +0200

    ejercicio 04 push

--- modulo01/arrays/ejercicio04.js
+++ modulo01/arrays/ejercicio04.js
@@ -2,7 +2,7 @@
 
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 rickAndMortyCharacters.push("Morty","Summer");
-let position = rickAndMortyCharacters.length -1;
+let position = rickAndMortyCharacters.length-1;
 
 console.log(rickAndMortyCharacters[position]);
 
